dee79757895145c5e14cfb55f71a55f26922d47f
Nolan Brubaker <nolan@nbrubaker.com>
2025-10-31 15:31:46 -0400

    Fix shell
    Signed-off-by: Nolan Brubaker <nolan@nbrubaker.com>

--- gh_reviews.sh
+++ gh_reviews.sh
@@ -1,4 +1,4 @@
-#! /bin/zh
+#! /bin/zsh
 
 # Fetch PR review requests that are open
 gh search prs --review-requested=@me --state=open --sort updated --json repository,number,title,updatedAt > ${HOME}/.pkm_cache/gh_reviews.json
